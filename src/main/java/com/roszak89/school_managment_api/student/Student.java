584e6df99db24a8199cf1f73f8b26e33a60e86e8
markross89 <roszak89@gmail.com>
2026-07-29 13:03:59 +0200

    feat(student): Add Student Entity and repository

---
+++ src/main/java/com/roszak89/school_managment_api/student/Student.java
@@ -0,0 +1,44 @@
+package com.roszak89.school_managment_api.student;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "class_id")
+//    private SchoolClass schoolClass;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+    
+}
